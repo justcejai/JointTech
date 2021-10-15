93790923f41e6974ee4458c19b567dcb792c2f22
Rene Montes <rmontes2@ncsu.edu>
2021-10-15 10:18:55 -0400

    Fixed issues causing page not to build, created basic homepage layout, created card component

--- react-jointtech/src/Home.js
+++ react-jointtech/src/Home.js
@@ -7,3 +7,21 @@ let language;
 let card, setCard;
 let matches, setMatches;
 
+class Home extends React.Component {
+    render() { 
+        return <div>
+            <div><h1>JointTech</h1></div>
+            <div className="" style={{backgroundColor: "#D3D3D3"}}>
+                <div className="d-flex justify-content-around " style={{display: 'flex', flexDirection: 'row'}}>
+                <Card header={"My Event Calendar"}/>
+                <Card header={"Event Swipe"}/>
+                <Card header={"Event Quickview"}/>
+                </div>
+                
+            </div>
+        </div>
+        ;
+    }
+}
+ 
+export default Home;
